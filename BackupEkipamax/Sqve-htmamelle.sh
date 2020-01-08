17ab4cd00f2a3b1b2b2b288539dfa18ece9df138
kah@Loyx <kahloyx@gmail,com>
2020-01-08 22:03:28 +0100

    ca commence a prendre forme

---
+++ BackupEkipamax/Sqve-htmamelle.sh
@@ -0,0 +1,15 @@
+sudo mkdir `date "+%d-%m-%Y_%H:%M"`
+cd `date "+%d-%m-%Y_%H:%M"`
+sudo wget "https://ekipamax.000webhostapp.com/index.html"
+sudo wget "https://ekipamax.000webhostapp.com/Home.html"
+sudo wget "https://ekipamax.000webhostapp.com/Spotify-web-player.html"
+sudo wget "https://ekipamax.000webhostapp.com/Spotify-2.html"
+sudo wget "https://ekipamax.000webhostapp.com/Spotify-3.html"
+sudo wget "https://ekipamax.000webhostapp.com/Spotify-4.html"
+sudo wget "https://ekipamax.000webhostapp.com/Spotify-5.html"
+sudo wget "https://ekipamax.000webhostapp.com/Spotify-6.html"
+sudo wget "https://ekipamax.000webhostapp.com/Spotify-7.html"
+sudo wget "https://ekipamax.000webhostapp.com/Spotify-8.html"
+sudo wget "https://ekipamax.000webhostapp.com/Pas-pour-toi.html"
+sudo wget "https://ekipamax.000webhostapp.com/digage.html"
+
